6543ee7186407cd7b05f311b55421a364d821455
w_zhangtianyong <w_zhangtianyong@xiwang.com>
2024-03-04 16:41:31 +0800

    测活接口

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// curl http://localhost:8989/ping
 // curl http://localhost:8989/deploy
 
 func main() {
 	engine := gin.New()
+	engine.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, "Welcome to zty project!")
+	})
 	engine.GET("/deploy", server.AutoDeploy)
 	err := engine.Run(":8989")
 	if err != nil {
